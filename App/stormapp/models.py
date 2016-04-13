5eeb8dae9e4ae1154666c10eb14b01c5a2a68b1d
Bruno Melo <brunousml@gmail.com>
2016-04-12 21:01:05 -0300

    Update stormapp models

--- App/stormapp/models.py
+++ App/stormapp/models.py
@@ -1,12 +1,33 @@
+import os
+
 from django.db import models
+
+def get_image_path(instance, filename):
+	return os.path.join('static/stormapp/movie_pictures', str(instance.category), filename)
+
 
 class Movie(models.Model):
 	title = models.CharField(max_length=200)
-	sinopse = models.CharField(max_length=800)
-	picture = models.TextField()
+	sinopse = models.TextField(max_length=800)
+	created_at = models.DateTimeField(auto_now_add=True)
+	picture = models.ImageField(upload_to='stormapp/static/stormapp/movie_pictures', blank=True, null=True)
+
+	def __str__(self):
+		return self.title
+
+
 
 class Actor(models.Model):
 	name =  models.CharField(max_length=200)
 
+	def __str__(self):
+		return self.name
+
+
+
+
 class Gender(models.Model):
 	name =  models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
